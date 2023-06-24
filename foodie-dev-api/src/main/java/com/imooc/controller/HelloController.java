d683e0538aec6db52be8928316d7da58bd1985e5
hanchaoyue <577833070@qq.com>
2023-06-24 16:47:37 +0800

    feat(ADD): 整和 log4j 打印日志

--- foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.imooc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
@@ -13,9 +15,14 @@ import springfox.documentation.annotations.ApiIgnore;
 @ApiIgnore
 @RestController
 public class HelloController {
+    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping(value = "/hello")
     public Object hello() {
+        logger.debug("hello");
+        logger.info("hello");
+        logger.warn("hello");
+        logger.error("hello");
         return "x";
     }
 
