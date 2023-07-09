6fb448eee49411fc18571d49a3325f00b5ea2074
hanchaoyue <577833070@qq.com>
2023-07-09 15:03:15 +0800

    feat(ADD): week2 5-6 购物车 - 删除

--- foodie-dev-api/src/main/java/com/imooc/controller/ShopcartController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/ShopcartController.java
@@ -34,8 +34,19 @@ public class ShopcartController {
         System.out.println(shopcartBO);
         // TODO  前端用户在登录的情况下，添加商品到购物车，会同时在后端同步购物车到redis缓存
         return IMOOCJSONResult.ok();
+    }
 
-
+    @ApiOperation(value = "从购物车中删除商品", notes = "从购物车中删除商品.", httpMethod = "POST")
+    @PostMapping(value = "/del")
+    public IMOOCJSONResult del(@RequestParam String userId,
+                               @RequestParam String itemSpecId,
+                               HttpServletRequest request,
+                               HttpServletResponse response) {
+        if (StringUtils.isBlank(userId) || StringUtils.isBlank(itemSpecId)) {
+            return IMOOCJSONResult.errorMsg("");
+        }
+        // TODO  用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的数据
+        return IMOOCJSONResult.ok();
     }
 
 }
